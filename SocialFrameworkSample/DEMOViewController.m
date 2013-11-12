2fa4641f327f9d8f60840390b7e19adb4faa3f00
kbfromindia <kbfromindia2@gmail.com>
2013-11-12 17:14:04 +0530

    Initial Commit

---
+++ SocialFrameworkSample/DEMOViewController.m
@@ -0,0 +1,29 @@
+//
+//  DEMOViewController.m
+//  SocialFrameworkSample
+//
+//  Created by Aditya on 12/11/13.
+//  Copyright (c) 2013 Aditya. All rights reserved.
+//
+
+#import "DEMOViewController.h"
+
+@interface DEMOViewController ()
+
+@end
+
+@implementation DEMOViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
